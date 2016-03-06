3090da33272cf4e4a095a452f14234149cc886a9
Tomas Bjerre <tomas.bjerre85@gmail.com>
2016-03-06 12:07:27 +0100

    Lint example

--- build.sh
+++ build.sh
@@ -6,6 +6,10 @@
 npm install -v
 ./node_modules/.bin/grunt
 
+#Lint
+mkdir -p build/reports/lint
+./node_modules/.bin/csslint web/css-file2.css --format=lint-xml > build/reports/lint/lint.xml
+
 #CPPLint
 #cppcheck --quiet --enable=all --force --inline-suppr --xml --xml-version=2 . 2> cppcheck-result.xml
 #cd ..
