a65136bd93a94a604f9ca09d9d570aa0a7ae1914
Tomas Bjerre <tomas.bjerre85@gmail.com>
2020-09-16 17:24:52 +0200

    asdsad

--- report-to-bitbucket-server.sh
+++ report-to-bitbucket-server.sh
@@ -5,7 +5,24 @@ npx violation-comments-to-bitbucket-server-command-line \
  -pk PROJ \
  -rs REPO \
  -prid 1 \
+ -keep-old-comments false \
  -v "CHECKSTYLE" "." ".*checkstyle/main\.xml$" "Checkstyle"
 
+
+npx violation-comments-to-bitbucket-server-command-line \
+ -server-url http://localhost:7990 \
+ -username admin \
+ -password admin \
+ -pk PROJ \
+ -rs REPO \
+ -prid 1 \
+ -keep-old-comments true \
+ -create-single-file-comments false \
+ -create-comment-with-all-single-file-comments true \
+ -comment-only-changed-files false \
+ -v "GENERIC" "." ".*eneric\.txt$" "Generic" \
+ -comment-template "
+message: {{{violation.message}}}
+"
+
 # -show-debug-info \
-# -create-single-file-comments-tasks true \
